7b42a3a0da5af6f794fbce11930a4cb14524059e
rhapsodyai <rhapsody.ai@gmail.com>
2017-05-22 23:37:39 -0700

    added java bloom filer

---
+++ BloomFilter/BloomFilter/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  BloomFilter
+//
+//  Created by Melissa Lynn Auclaire on 2017/05/21.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ BloomFilterJava/src/BloomFilter.java
@@ -0,0 +1,18 @@
+
+public class BloomFilter {
+
+	public static void main(String[] args) {
+		MurmurHash mh = new MurmurHash();
+		
+		/*Implement bloom filer spell checker */
+	}
+	
+	public void add() {
+		
+	}
+	
+	public void contains() {
+		
+	}
+
+}
